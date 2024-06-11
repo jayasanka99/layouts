c6d198a92d8a385155003c595aff7d5d18c85094
kusal jayasanka <jayasankakusal99@gmail.com>
2024-06-11 23:30:20 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xff9E00FF)
+        ),
+        body:Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Color(0xff06FFA5),
+              ),
+              Container(
+                height: 200,
+                width: double.infinity,
+                color: Color(0xff06FFA5),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 350,
+                    width: 175,
+                    color: Color(0xffFFE500),
+                  ),
+                  Container(
+                    height: 350,
+                    width: 175,
+                    color: Color(0xffFFE500),
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
