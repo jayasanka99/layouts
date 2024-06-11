38aeb98c93129a7a6e057661ce778dd7dab65c40
kusal jayasanka <jayasankakusal99@gmail.com>
2024-06-11 23:53:31 +0530

    add app bar title

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          title: Text("Layout 2"),
           backgroundColor: const Color(0xff9E00FF)
         ),
         body:Padding(
