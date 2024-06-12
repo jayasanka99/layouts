e18c78c495d11db31c4bf0bab725790755bad872
kusal jayasanka <jayasankakusal99@gmail.com>
2024-06-12 15:34:19 +0530

    quite advance layout with scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -13,43 +13,186 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xff9E00FF)
-        ),
-        body:Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                height: 200,
-                width: double.infinity,
-                color: Color(0xff06FFA5),
-              ),
-              Container(
-                height: 200,
-                width: double.infinity,
-                color: Color(0xff06FFA5),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          appBar: AppBar(backgroundColor: const Color(0xff9E00FF)),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Container(
-                    height: 350,
-                    width: 175,
-                    color: Color(0xffFFE500),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: (20.0), vertical: 25.0),
+                      height: 350,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: const Color(0xff06FFA5),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Flutter is an open-source",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Text(
+                            "Flutter is an open-source UI (User Interface) software development kit created by Google. "
+                            "It is used to build natively compiled applications for mobile, web, and desktop from a single codebase. "
+                            "Flutter was first introduced in 2015.",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w400),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                height: 150,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.deepPurpleAccent,
+                                ),
+                              ),
+                              Container(
+                                height: 150,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.deepPurpleAccent,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
                   ),
+                  SizedBox(height: 20,),
                   Container(
-                    height: 350,
-                    width: 175,
-                    color: Color(0xffFFE500),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: (20.0), vertical: 25.0),
+                    height: 570,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xffFFE500),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Flutter is an open-source",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            // SizedBox(width: 50,),
+                            Icon(
+                              Icons.shopping_bag_outlined,
+                              size: 20,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "Flutter is an open-source UI (User Interface) software development kit created by Google. "
+                          "It is used to build natively compiled applications for mobile, web, and desktop from a single codebase. "
+                          "Flutter was first introduced in 2015.",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w400),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 330,
+                          width: 400,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: const Color(0xff9E00FF),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Column(
+                              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Flutter is an open-source",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Text(
+                                  "Flutter is an open-source UI (User Interface) software development kit created by Google. "
+                                  "It is used to build natively compiled applications for mobile, web, and desktop from a single codebase. "
+                                  "Flutter was first introduced in 2015.",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.white),
+                                ),
+                                const SizedBox(
+                                  height: 40,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        height: 90,
+                                        width:90,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(100),
+                                          color: const Color(0xff06FFA5),
+                                        ),
+                                      ),Container(
+                                        height: 90,
+                                        width:90,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(100),
+                                          color: const Color(0xff06FFA5),
+                                        ),
+                                      ),Container(
+                                        height: 90,
+                                        width:90,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(100),
+                                          color: const Color(0xff06FFA5),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ],
-              )
-            ],
-          ),
-        )
-      ),
+              ),
+            ),
+          )),
     );
   }
 }
